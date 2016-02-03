bd08cd0a1aee060e6c789744df5d6e78e8fda7f8
Petra Gulicher <petra@ustwo.com>
2016-02-03 14:22:59 +1100

    Swap the vanilla JS modal for colorbox :sadface:

--- Gemfile
+++ Gemfile
@@ -1,7 +1,4 @@
 source 'https://rubygems.org'
 
 gem 'jekyll'
-
-group :jekyll_plugins do
-  gem 'jekyll-browserify'
-end
+gem 'jekyll-coffeescript'
--- js/main.js
+++ js/main.js
@@ -1,16 +1,64 @@
 ---
 ---
 
-var Forms = require('./js/app/forms.js');
-var Modals = require('./js/app/modals.js');
+(function(document) {
 
+/**
+ * Forms
+ */
+var Forms = {
+	init: function() {
+		this.input = document.querySelector('.signup-input');
+		this.submit = document.querySelector('.signup-submit');
+
+		var self = this;
+		this.submit.addEventListener('click', self.validate.bind(self));
+	},
+	validate: function() {
+		if (!this.input.checkValidity()) {
+			// Do error
+		} else {
+			this.submitForm();
+		}
+	},
+	getParams: function() {
+		return params = window.location.search.length ? window.location.search.replace(/(^\?)/,'').split('&').map(function(i) {
+			var node = {};
+			node[i.split('=')[0]] = i.split('=')[1];
+			return node;
+		}) : null;
+	},
+	submitForm: function() {
+		var url = this.submit.dataset.href + '?email=' + this.input.value;
+		var params = this.getParams();
+		if (params[0] && params[0].hasOwnProperty('results')) {
+			url += '&results=true';
+		}
+		window.location = url;
+	}
+};
 Forms.init();
+
+/**
+ * Modals
+ */
+
+var Modals = {
+	init: function() {
+
+		var modalWidth = $(window).width() > 768 ? '50%' : '90%';
+
+		$('a[rel="modal:open"]').colorbox({
+			inline: true,
+			href: '#signup',
+			width: modalWidth
+		});
+
+	}
+};
 Modals.init();
 
-// (function(document) {
-
-// }(document));
-
+}(document));
 
 /*
 
@@ -51,8 +99,3 @@ for each bizzo
 	sort bizzos by the difference value and display top 20
 
 */
-
-
-
-
-
